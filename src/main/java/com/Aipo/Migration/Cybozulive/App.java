b5db801aaf7b5f5b4e9af099cb13e79701c2d7c8
nnnobuo <akiyamajyoni0922@gmail.com>
2019-01-08 18:17:00 +0900

    読み込んだcsvをAipo用に並び替え、パスワードを自動生成
    [チケットURL]
    https://trello.com/c/JLA0QEEG
    
    [詳細]
    nanoTimeメソッドを使ったパスワードの生成と読み込んだ情報をAipo用に並び替えたものに生成できるようにしました。ただ残りの課題としてはAipoでインポートする際は50件ごとにしかインポートすることができないので、それができるようにするために50件ごとにファイルを分割する方法をまだ実装中です。

--- src/main/java/com/Aipo/Migration/Cybozulive/App.java
+++ src/main/java/com/Aipo/Migration/Cybozulive/App.java
@@ -1,25 +1,62 @@
 package com.Aipo.Migration.Cybozulive;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.util.Random;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
-public class App {
-	public static void main(String[] args) {
-		try {
-			Reader in = new FileReader("file.csv");
-			Iterable<CSVRecord> records =
-				CSVFormat.EXCEL.withHeader().parse(in);
-			for (CSVRecord record : records) {
-			    String lastName = record.get("Last Name");
-			    String firstName = record.get("First Name");
 
-			    System.out.print (lastName + " ");
-			    System.out.println (firstName);
+
+
+public class App {
+	private static final String charset =
+			"!0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	public static String generatePassword(int length) {
+		Random rand = new Random(System.nanoTime());
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < length; i++) {
+			int pos = rand.nextInt(charset.length());
+			sb.append(charset.charAt(pos));
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+        try {
+        	BufferedWriter bw_50 = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(
+                                    new File("Aipo用0~50.csv")), "shift-jis"));
+
+        	//Aipo用に変換するファイルを引数の記述する
+        	Reader in = new FileReader("utf-8.csv");
+			Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(in);
+            CSVPrinter printer = CSVFormat.EXCEL.print(bw_50);
+			for (CSVRecord record : records) {
+
+				String lastName = record.get("姓");
+			    String firstName = record.get("名");
+			    String kanaLastName = record.get("よみがな姓");
+			    String kanaFirstName = record.get("よみがな名");
+			    String email = record.get("メールアドレス");
+
+			    //Aipoインポート必須項目
+			    //メールアドレス、パスワード、姓、名、かな姓、かな名
+
+			    //csv書き込みテスト
+			    printer.printRecord(email, generatePassword(8), lastName, firstName, kanaLastName, kanaFirstName);
+
 			}
+
 		} catch (IOException e) {
 			System.out.println(e);
 		}

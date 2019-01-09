8a168556cae09ba1093268bbed799f41b354bb7a
nnnobuo <akiyamajyoni0922@gmail.com>
2019-01-09 18:26:05 +0900

    エクスポートしたファイルのレコードが50件以上だったもののファイルの分割とAipoでインポートできるような型にエクスポート
    [チケットURL]
    https://trello.com/c/JLA0QEEG
    
    [詳細]
    エクスポートしたファイルのレコードが50件以上だったもののファイルの分割とAipoでインポートできるようなに成型しました。取得したレコードを一括で書き込みをするとファイルを読み込み部分の記述と書き込み部分の定義などのスコープの問題があったのですが、CSVファイルを読み込んで書き込むを行う前に、事前に50件ずつ分割することで書き込みのスコープの問題は解決しました。

--- src/main/java/com/Aipo/Migration/Cybozulive/App.java
+++ src/main/java/com/Aipo/Migration/Cybozulive/App.java
@@ -7,9 +7,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
@@ -17,11 +20,10 @@ import org.apache.commons.csv.CSVRecord;
 
 
 public class App {
-	private static final String charset =
-			"!0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+	//パスワード生成
 	public static String generatePassword(int length) {
 		Random rand = new Random(System.nanoTime());
+		String charset = "!0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 		StringBuffer sb = new StringBuffer();
 		for (int i = 0; i < length; i++) {
 			int pos = rand.nextInt(charset.length());
@@ -32,30 +34,164 @@ public class App {
 
 	public static void main(String[] args) {
         try {
-        	BufferedWriter bw_50 = new BufferedWriter(
-                    new OutputStreamWriter(
-                            new FileOutputStream(
-                                    new File("Aipo用0~50.csv")), "shift-jis"));
-
         	//Aipo用に変換するファイルを引数の記述する
         	Reader in = new FileReader("utf-8.csv");
-			Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader().parse(in);
-            CSVPrinter printer = CSVFormat.EXCEL.print(bw_50);
-			for (CSVRecord record : records) {
 
-				String lastName = record.get("姓");
-			    String firstName = record.get("名");
-			    String kanaLastName = record.get("よみがな姓");
-			    String kanaFirstName = record.get("よみがな名");
-			    String email = record.get("メールアドレス");
+        	//読み込んだCSVファイルをフォーマット
+		    CSVParser parser = CSVFormat
+			        .EXCEL
+			        .withIgnoreEmptyLines(true)
+			        .withFirstRecordAsHeader()
+			        .withIgnoreSurroundingSpaces(true)
+			        .parse(in);
 
-			    //Aipoインポート必須項目
-			    //メールアドレス、パスワード、姓、名、かな姓、かな名
+        	//読み込んだCSVをListに代入
+	    	List<CSVRecord> recordList = parser.getRecords();
 
-			    //csv書き込みテスト
-			    printer.printRecord(email, generatePassword(8), lastName, firstName, kanaLastName, kanaFirstName);
+		    //ファイルの生成(if文で配列の要素数によってファイルを作成する)
+			    BufferedWriter bw_50 = new BufferedWriter(
+	                    new OutputStreamWriter(
+	                            new FileOutputStream(
+	                                    new File("Aipo用0~50.csv")), "Shift-JIS"));
 
-			}
+			    BufferedWriter bw_100 = new BufferedWriter(
+	                    new OutputStreamWriter(
+	                            new FileOutputStream(
+	                                    new File("Aipo用51~100.csv")), "Shift-JIS"));
+
+			    BufferedWriter bw_150 = new BufferedWriter(
+	                    new OutputStreamWriter(
+	                            new FileOutputStream(
+	                                    new File("Aipo用101~150.csv")), "Shift-JIS"));
+
+			    BufferedWriter bw_200 = new BufferedWriter(
+	                    new OutputStreamWriter(
+	                            new FileOutputStream(
+	                                    new File("Aipo用151~200.csv")), "Shift-JIS"));
+
+			    BufferedWriter bw_250 = new BufferedWriter(
+	                    new OutputStreamWriter(
+	                            new FileOutputStream(
+	                                    new File("Aipo用201~250.csv")), "Shift-JIS"));
+
+			    BufferedWriter bw_300 = new BufferedWriter(
+	                    new OutputStreamWriter(
+	                            new FileOutputStream(
+	                                    new File("Aipo用251~300.csv")), "Shift-JIS"));
+
+		    //プリンターの生成
+        	CSVPrinter printer_50 = CSVFormat.EXCEL.print(bw_50);
+        	CSVPrinter printer_100 = CSVFormat.EXCEL.print(bw_100);
+        	CSVPrinter printer_150 = CSVFormat.EXCEL.print(bw_150);
+        	CSVPrinter printer_200 = CSVFormat.EXCEL.print(bw_200);
+        	CSVPrinter printer_250 = CSVFormat.EXCEL.print(bw_250);
+        	CSVPrinter printer_300 = CSVFormat.EXCEL.print(bw_300);
+
+	    	//Aipoのインポート用に50件ずつListに代入
+	    	List<CSVRecord> list_50 = new ArrayList<CSVRecord>();
+	    	List<CSVRecord> list_100 = new ArrayList<CSVRecord>();
+	    	List<CSVRecord> list_150 = new ArrayList<CSVRecord>();
+	    	List<CSVRecord> list_200 = new ArrayList<CSVRecord>();
+	    	List<CSVRecord> list_250 = new ArrayList<CSVRecord>();
+	    	List<CSVRecord> list_300 = new ArrayList<CSVRecord>();
+
+	    	//インポートした要素を分割
+		    for(CSVRecord s: recordList) {
+		    	if (s.getRecordNumber() < 51) {
+		    		list_50.add(s);
+		    	} else if (s.getRecordNumber() > 50 && s.getRecordNumber() < 101) {
+		    		list_100.add(s);
+		    	} else if (s.getRecordNumber() > 100 && s.getRecordNumber() < 151) {
+		    		list_150.add(s);
+		    	} else if (s.getRecordNumber() > 150 && s.getRecordNumber() < 201) {
+		    		list_200.add(s);
+		    	} else if (s.getRecordNumber() > 200 && s.getRecordNumber() < 251) {
+		    		list_250.add(s);
+		    	} else if (s.getRecordNumber() > 250 && s.getRecordNumber() < 301) {
+		    		list_300.add(s);
+		    	}
+		    }
+
+		    if (list_50.size() > 0) {
+				  for(CSVRecord s: list_50) {
+					    String lastName = s.get("姓");
+					    String firstName = s.get("名");
+					    String kanaLastName = s.get("よみがな姓");
+					    String kanaFirstName = s.get("よみがな名");
+					    String email = s.get("メールアドレス");
+					    printer_50.printRecord(email, generatePassword(8), lastName, firstName, kanaLastName, kanaFirstName);
+				  }
+				  printer_50.close();
+		    }
+
+		    if (list_100.size() > 0) {
+				  for(CSVRecord s: list_100) {
+					    String lastName = s.get("姓");
+					    String firstName = s.get("名");
+					    String kanaLastName = s.get("よみがな姓");
+					    String kanaFirstName = s.get("よみがな名");
+					    String email = s.get("メールアドレス");
+					    printer_100.printRecord(email, generatePassword(8), lastName, firstName, kanaLastName, kanaFirstName);
+				  }
+				  printer_100.close();
+		    }
+
+		    if (list_150.size() > 0) {
+				  for(CSVRecord s: list_150) {
+					    String lastName = s.get("姓");
+					    String firstName = s.get("名");
+					    String kanaLastName = s.get("よみがな姓");
+					    String kanaFirstName = s.get("よみがな名");
+					    String email = s.get("メールアドレス");
+					    printer_150.printRecord(email, generatePassword(8), lastName, firstName, kanaLastName, kanaFirstName);
+				  }
+				  printer_150.close();
+		    }
+
+		    if (list_200.size() > 0) {
+				  for(CSVRecord s: list_200) {
+					    String lastName = s.get("姓");
+					    String firstName = s.get("名");
+					    String kanaLastName = s.get("よみがな姓");
+					    String kanaFirstName = s.get("よみがな名");
+					    String email = s.get("メールアドレス");
+					    printer_200.printRecord(email, generatePassword(8), lastName, firstName, kanaLastName, kanaFirstName);
+				  }
+				  printer_200.close();
+		    }
+
+		    if (list_250.size() > 0) {
+				  for(CSVRecord s: list_250) {
+					    String lastName = s.get("姓");
+					    String firstName = s.get("名");
+					    String kanaLastName = s.get("よみがな姓");
+					    String kanaFirstName = s.get("よみがな名");
+					    String email = s.get("メールアドレス");
+					    printer_250.printRecord(email, generatePassword(8), lastName, firstName, kanaLastName, kanaFirstName);
+				  }
+				  printer_250.close();
+		    }
+
+		    if (list_300.size() > 0) {
+				  for(CSVRecord s: list_300) {
+					    String lastName = s.get("姓");
+					    String firstName = s.get("名");
+					    String kanaLastName = s.get("よみがな姓");
+					    String kanaFirstName = s.get("よみがな名");
+					    String email = s.get("メールアドレス");
+					    printer_300.printRecord(email, generatePassword(8), lastName, firstName, kanaLastName, kanaFirstName);
+				  }
+				  printer_300.close();
+		    }
+
+
+
+			    System.out.println(list_50.size());
+			    System.out.println(list_100.size());
+			    System.out.println(list_150.size());
+			    System.out.println(list_200.size());
+			    System.out.println(list_250.size());
+			    System.out.println(list_300.size());
 
 		} catch (IOException e) {
 			System.out.println(e);

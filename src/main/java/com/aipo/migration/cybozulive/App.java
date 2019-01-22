37fd30277f40041cdf411156237f2697c508ea58
nnnobuo <akiyamajyoni0922@gmail.com>
2019-01-22 15:00:26 +0900

    「場所」も51文字以上の場合はでトリミング
    [チケットURL]
    https://trello.com/c/Yl7PE5QI
    
    [詳細]
    場所が51文字以上だった場合、最後の","で区切る

--- src/main/java/com/aipo/migration/cybozulive/App.java
+++ src/main/java/com/aipo/migration/cybozulive/App.java
@@ -221,7 +221,7 @@ public class App {
                                     title = "---";
                                 }
                             }
-                            // 最大99文字
+                            // 最大50文字
                             if (title.length() > 51) {
                                 title = title.substring(0, 50);
                             }
@@ -232,12 +232,16 @@ public class App {
                                     memo = "";
                                 }
                             }
+
                             if (s.isSet("設備")) {
                                 facility = s.get("設備");
                             }
-                            // 最大99文字
-                            if (facility.length() > 100) {
-                                facility = facility.substring(0, 99);
+                            // 複数個登録されている施設を","で区切る
+                            if (facility.length() > 51) {
+                                facility = facility.substring(0, 50);
+                                StringBuilder sbfacility = new StringBuilder(facility);
+                                int indexNum = sbfacility.lastIndexOf(",");
+                                facility = facility.substring(0, indexNum);
                             }
                             if (s.isSet("参加者")) {
                                 userName = s.get("参加者");
